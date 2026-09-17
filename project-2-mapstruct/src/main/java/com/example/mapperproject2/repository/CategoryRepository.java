fe86b4dc74c24039477ce6ceff2f41af0654bb03
LamNhatAn <LamNhatAn@users.noreply.github.com>
2026-09-17 15:31:09 +0700

    Submit assignment 4 only

--- project-2-mapstruct/src/main/java/com/example/mapperproject2/repository/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.mapperproject2.repository;
-
-import com.example.mapperproject2.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> { }
--- project-2-mapstruct/src/main/java/com/example/mapperproject2/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.example.mapperproject2.service;
-
-import com.example.mapperproject2.dto.ProductDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface ProductService { Page<ProductDto> findAll(String keyword, Pageable pageable); ProductDto findById(Long id); ProductDto save(ProductDto dto); void deleteById(Long id); }
